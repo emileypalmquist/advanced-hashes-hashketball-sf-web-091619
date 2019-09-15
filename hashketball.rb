55609fad1a5344744f6b25177d89fb62bf6b47ba
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:13:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,38 +4,50 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        { :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => 
+        { :number => '0',
+          :shoe => '16',
+          :points => '22',
+          :rebounds => '12',
+          :assists => '12',
+          :steals => '3',
+          :blocks => '1',
+          :slam_dunks => '1' 
         }, 
-        { :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => }
+        { :number => '30',
+          :shoe => '14',
+          :points => '12',
+          :rebounds => '12',
+          :assists => '12',
+          :steals => '12',
+          :blocks => '12',
+          :slam_dunks => '7'
+          
+        }
       ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
-        { :number => ,
-          :shoe => ,
-          :points => ,
-          :rebounds => ,
-          :assists => ,
-          :steals => ,
-          :blocks => ,
-          :slam_dunks => },
-        {}
+        { :number => '4',
+          :shoe => '18',
+          :points => '10',
+          :rebounds => '1',
+          :assists => '1',
+          :steals => '2',
+          :blocks => '7',
+          :slam_dunks => '2'
+          
+        },
+        { :number => '0',
+          :shoe => '16',
+          :points => '12',
+          :rebounds => '4',
+          :assists => '7',
+          :steals => '22',
+          :blocks => '15',
+          :slam_dunks => '10'
+        }
       ]
     }
   }
