cd4e2184de6109f69d1fe4883dfb49ff9b5b2be4
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:50:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,7 +36,7 @@ def game_hash
         { :name => "Jeff Adrien",
           :number => '4',
           :shoe => '18',
-          :points => '10',
+          :points => 10,
           :rebounds => '1',
           :assists => '1',
           :steals => '2',
