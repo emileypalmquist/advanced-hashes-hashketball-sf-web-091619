bc8b71ac536f059e96cb83930614d71b6c6491d7
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:08:54 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,7 +27,14 @@ def game_hash
       :team_name => "Charlotte Hornets",
       :colors => ["Turquoise", "Purple"],
       :players => [
-        {},
+        { :number => ,
+          :shoe => ,
+          :points => ,
+          :rebounds => ,
+          :assists => ,
+          :steals => ,
+          :blocks => ,
+          :slam_dunks => },
         {}
       ]
     }
