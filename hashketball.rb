c9f495d150397f07cbe62728a514f1d79e9fb499
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 02:40:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,7 +194,7 @@ def most_points_scored
       points = player[:points]
       if points > most_points
         most_points = points
-        mvp = player[:player_name]
+        mvp = player[:name]
       end
     end
   end
