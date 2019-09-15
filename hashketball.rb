1f24a6782c5f1cc6e5b79929f6ed810201b659db
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:52:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -84,6 +84,16 @@ def game_hash
           :blocks => '15',
           :slam_dunks => '10'
         },
+        { :name => 'Alan Anderson',
+          :number => '0',
+          :shoe => '16',
+          :points => 22,
+          :rebounds => '12',
+          :assists => '12',
+          :steals => '3',
+          :blocks => '1',
+          :slam_dunks => '1' 
+        },
         
       ]
     }
