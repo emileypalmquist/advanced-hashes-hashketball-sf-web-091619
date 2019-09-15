d49949d2471f9bdba2112f559e8b2073ad6cd4fc
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:46:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -39,8 +39,8 @@ def game_hash
           :points => 10,
           :rebounds => 1,
           :assists => 1,
-          :steals => 2,
-          :blocks =>' 7',
+          :steals => '2',
+          :blocks => '7',
           :slam_dunks => '2'
           
         },
