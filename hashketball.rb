61df6a19051f8f7167f96180acab4b7793a56085
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:00:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,17 @@
-# Write your code here!
+def game_hash
+  hash = {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => [],
+      :players =>
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ,
+      :players => 
+    }
+  }
+end
 
 
 
