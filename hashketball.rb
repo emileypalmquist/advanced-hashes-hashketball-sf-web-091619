c521e1f09a40ca2f1a03517315c3f5eb67beb0a7
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:35:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -62,7 +62,7 @@ end
 
 
 def num_points_scored(player_name)
-  game_hash.each do |location, team_data|
+  game_hash.each do |location, players|
     binding.pry
     team_data.each do |attribute, data|
       binding.pry
