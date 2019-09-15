f3a109fcd037666e9fa0a398307acf34fb5adb5a
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 01:15:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
           :blocks => '1',
           :slam_dunks => '1' 
         }, 
-        { :name => 'Bismack Biyombo'
+        { :name => ''
           :number => '30',
           :shoe => '14',
           :points => '12',
